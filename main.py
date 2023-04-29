b91c248f14af1dd32e40752ad2cba2edd1e334e7
dany1940 <59851426+dany1940@users.noreply.github.com>
2023-04-29 10:29:22 +0100

    Created functional code to find the pattern

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from arguments import get_arguments
 import re
 
 
+
 def read_file(args):
     """Read the file and return a list of sentences with no new line characters."""
     path = args.P
-    # file_size = os.path.getsize('nodata.txt')
+    #file_size = os.path.getsize('nodata.txt')
+    sentences = []
     try:
-        with open(path, "r") as file:
-            sentence = file.read().splitlines()
+       with open(path, 'r') as file:
+            lines = file.read().splitlines()
+            for sentence in lines:
+                sentence =  re.sub('[^A-Za-z]+', ' ', sentence)
+                sentence = sentence.strip()
+                sentences.append(sentence)
     except IndexError:
         print("The index is out of range!")
         raise SystemExit(1)
@@ -19,35 +25,30 @@ def read_file(args):
     except PermissionError:
         print("You do not have permission to access this file!")
         raise SystemExit(1)
-    return sentence
+    return sentences
 
-
-def get_all_patterns(read_file):
+def get_all_patterns(sentence):
     """get a list of all possible substrings of the pattern"""
-    pattern = read_file.pop()
-    list_of_substrings = [
-        pattern[i:j]
-        for i in range(len(pattern))
-        for j in range(i + 1, len(pattern) + 1)
-    ]
-    return list_of_substrings
-
+    pattern = sentence[-1]
+    #list_of_substrings = ([pattern[i:j] for i in range(len(pattern)) for j in range(i + 1, len(pattern) + 1)])
+    return pattern
 
 def find_pattern(pattern, sentences):
     """Find the pattern in the sentences."""
-    list_of_sentences = []
+    sentence = ("[{0}]".format(
+                       ''.join(map(str, sentences))))
     for sentence in sentences:
-        for pattern in pattern:
             if re.search(pattern, sentence):
-                list_of_sentences.append(sentence)
-    return list_of_sentences
+                print (("[{0}]".format(
+                       ''.join(map(str, sentence)))))
 
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
+    """Main function."""
+    """Read the file and return a list of sentences with no new line characters."""
     sentences = read_file(get_arguments())
-    new_sentences = []
-    for sentence in sentences:
-        sentence = "".join(ch for ch in sentence if ch.isalpha())
-        new_sentences.append(sentence)
-    print(new_sentences)
     pattern = get_all_patterns(sentences)
+    find_pattern(pattern, sentences[ : -1])
+
